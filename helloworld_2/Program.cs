7f548fd015795ea8c4b6aacf31f1ec4307bd0d1c
skywayoo <dorgonway@gmail.com>
2021-10-16 23:24:50 +0800

    add bad girl

--- helloworld_2/Program.cs
+++ helloworld_2/Program.cs
@@ -14,7 +14,10 @@ namespace helloworld
             string msg = "Hi, hello~超級辣妹";
             string user; //宣告誰是超級辣妹
             user = "clara";
+            string bad_user; //宣告誰是超級大壞蛋
+            bad_user =  "clara wang;
             Console.WriteLine("1."+msg+ user);
+            Console.WriteLine("壞透了的"+bad_user);
             /*test comment*/
         }
     }
