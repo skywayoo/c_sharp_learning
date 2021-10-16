1f07d185a6aaf33cdbb15aef5d717f1b1a7be853
skywayoo <dorgonway@gmail.com>
2021-10-16 16:57:31 +0800

    helloworld2

---
+++ helloworld_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Hi, hello~超級辣妹";
+            Console.WriteLine("1."+msg);
+            /*test comment*/
+        }
+    }
+}
